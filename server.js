2c8341956108814c79ba4b74741fe3f9fcfcbc98
Abdulwasiu <abdayoade@gmail.com>
2024-01-07 13:50:39 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const PORT = 3500;
+
+// middleware for static files
+app.use("/", express.static(path.join(__dirname, "public")));
+
+//routes
+app.use("/", require("./routes/root"));
+
+// catching pages or requests not found
+app.use("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 not found" });
+  } else {
+    res.type(txt).send("404 Not Found");
+  }
+});
+
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
